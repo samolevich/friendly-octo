aad35b1d0a3817a0551a6d4f39229f93468b0160
samolevich <samolevich@gmail.com>
2022-08-11 01:56:04 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,21 @@
-import axios from "axios";
-import { Button, Divider, List } from "antd";
-import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../actions";
-import { URL } from "../constants";
-import Table from "./Table";
+import { Divider, Row, Col } from "antd";
+
+import Board from "./Board";
+import ButtonLoader from "./ButtonLoader";
 const App = () => {
-  const dispatch = useDispatch();
-  const users = useSelector(state => state.usersReducer.users);
-
-  const onSubmitGetUsers = async () => {
-    const res = await axios.get(URL);
-    dispatch(getUsers(res.data.results));
-  };
-
   return (
     <>
-      <Button onClick={() => onSubmitGetUsers()}>Default Button</Button>
-      <Divider orientation="left">Users List</Divider>
-      <List
-        size="small"
-        bordered
-        dataSource={users}
-        renderItem={item => <List.Item>{item.name.first}</List.Item>}
-      />
-      <Table />
+      <Row justify="center" gutter={[110, 444]}>
+        <ButtonLoader />
+      </Row>
+
+      <Divider>User List</Divider>
+
+      <Row>
+        <Col lg={{ span: 18, offset: 3 }} md={24} xs={24}>
+          <Board />
+        </Col>
+      </Row>
     </>
   );
 };
